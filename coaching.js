a9c2394c8a625f3bddd81afc55da76b33eb90e17
tutorskhulna <125726178+tutorskhulna@users.noreply.github.com>
2023-02-21 14:42:27 +0600

    Update coaching.js

--- coaching.js
+++ coaching.js
@@ -1,46 +1,88 @@
+// Get the cart element
+const cart = document.getElementById('cart');
 
-fetch("coaching.json")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (coaching) {
-        let placeholder = document.querySelector("#data-output");
-        let out = "";
-        for (let tutor of coaching) {
-            out += `
-    <hr>
+// Load the teachers data from the JSON file
+fetch('coaching.json')
+  .then(response => response.json())
+  .then(data => {
+    // Loop through each teacher and create a card for them
+    data.forEach(teacher => {
+      // Create a new card element
+      const card = document.createElement('div');
+      card.classList.add('teacher-card');
 
-    <div class="cards">
-      
-  
-      <div class="cards-box">
-        <h1 class="cards-title">${tutor.name}</h1>
-        <h3 class="cards-title" style="color:blue;font-size:18px;">${tutor.place}</h3>
-       <br>
-        <div class="cards-content">
-         <h3>For Class:<br>${tutor.class}</h3>
-         <h3>Subject:<br>${tutor.subject}</h3>
-            <h3>Phone: ${tutor.phone}</h3>
-            </div>
-      </div>
-  
-      <div class="cards-footer">
-        
-        <a href="${tutor.fb}">facebook id link</a>
-        <a href="${tutor.email}">Email</a>
-        <hr>
-      <hr>
-      <h2 class="point">Satisfactory Point: ${tutor.point}</h2>
-      </div>
-      
-    </div>
- 
+      // Add the teacher's image to the card
+
+
+      // Add the teacher's name to the card
+
+      const name = document.createElement('h1');
+      name.textContent = teacher.name;
+      card.appendChild(name);
 
 
 
+      // Add the teacher's qualification to the card
+      const qualification = document.createElement('h2');
+      qualification.textContent = teacher.qualification;
+      card.appendChild(qualification);
 
-`;
-        }
+      // Add the teacher's subject to the card
+      const subjecttext = document.createElement('h4');
+      subjecttext.textContent = '#Subject: ';
+      card.appendChild(subjecttext);
 
-        placeholder.innerHTML = out;
+      const subject = document.createElement('p');
+      subject.textContent = teacher.subject;
+      card.appendChild(subject);
+
+      // Add the teacher's subject to the card
+      const placetext = document.createElement('h4');
+      placetext.textContent = '#Tutors location: ';
+      card.appendChild(placetext);
+
+
+      const place = document.createElement('p');
+      place.textContent = teacher.place;
+      card.appendChild(place);
+
+      // Add the teacher's subject to the card
+
+      const salarytext = document.createElement('h4');
+      salarytext.textContent = '#Expected salary /12 days: ';
+      card.appendChild(salarytext);
+
+      const salary = document.createElement('p');
+      salary.textContent = teacher.salary;
+      card.appendChild(salary);
+
+
+
+      const phonetext = document.createElement('h4');
+      phonetext.textContent = '#Phone no: ';
+      card.appendChild(phonetext);
+
+      const phone = document.createElement('p');
+      phone.textContent = teacher.phone;
+      card.appendChild(phone);
+
+      // Add a clickable link to the teacher's Facebook profile to the card
+      const fb = document.createElement('a');
+      fb.href = teacher.fb;
+      fb.textContent = 'Facebook Id Link';
+      fb.target = '_blank'; // Open the link in a new tab
+      const facebookId = document.createElement('p');
+      facebookId.textContent = '';
+      facebookId.appendChild(fb);
+      card.appendChild(facebookId);
+
+      // Add a button to add the teacher to the cart
+      const point = document.createElement('h3');
+      point.textContent = 'Satisfactory Point: ' + teacher.point;
+      card.appendChild(point);
+
+      // Add the card to the teachers list
+      const teachersList = document.getElementById('teachers-list');
+      teachersList.appendChild(card);
     });
+  });
